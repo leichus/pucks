5114df210df5837ad94a0d01f1160bc96ab3c1ec
Daniel Leichus <dpleichus@gmail.com>
2019-11-01 13:58:25 -0400

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ["react"],
+  extends: [
+    "eslint:recommended",
+  ],
+  parser: "babel-eslint",
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './css/style.css';
 // Log message to console
 logMessage('Welcome to Expack!');
 
-if (typeof(module.hot) !== 'undefined') {
-  module.hot.accept();
+if (typeof(module.hot) !== 'undefined') { // eslint-disable-line no-undef
+  module.hot.accept(); // eslint-disable-line no-undef
 }
